f3faa800848eae447eace9e3c7a9a9e6d11e95d0
annakatla <amanda.orrbeck@iths.se>
2022-04-07 15:19:51 +0200

    added a unfinished OrderController

---
+++ AlmedalGameStoreWeb/Areas/Guest/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using AlmedalGameStore.DataAccess.GenericRepository.IGenericRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmedalGameStoreWeb.Areas.Guest.Controllers
+{
+    [Area("Guest")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
